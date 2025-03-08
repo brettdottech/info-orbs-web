6bf9ecc73232fae2cf82cf638a649786685c8c8b
Christian Erpelding <erpelding@ce-data.de>
2025-03-08 14:10:05 +0100

    Add React StrictMode to root rendering
    Wrap the application in React's StrictMode to assist with identifying potential problems in the codebase. This change helps enforce best practices and improves overall code quality during development.

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,18 @@ import App from "./App";
 import {BrowserRouter as Router} from "react-router-dom";
 import {AuthProvider} from "./context/AuthContext";
 import "./styles/index.css";
+import {StrictMode} from "react";
 
 const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
 
 
 root.render(
-    <Router>
-        <AuthProvider>
-            <App/>
-        </AuthProvider>
-    </Router>
+    <StrictMode>
+        <Router>
+            <AuthProvider>
+                <App/>
+            </AuthProvider>
+        </Router>
+    </StrictMode>
 );
