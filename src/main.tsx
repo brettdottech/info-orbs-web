be054cee28212748886efd98f18742f61536d3fd
Christian Erpelding <flattermann@users.noreply.github.com>
2025-03-18 20:18:19 +0100

    Use config file for redirect URI in KindeProvider
    Replaced hardcoded redirect URI with dynamic value from the config file. This improves maintainability and allows configuration changes without modifying the code.

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import {BrowserRouter as Router} from "react-router-dom";
 import "./styles/global.css";
 import {KindeProvider} from "@kinde-oss/kinde-auth-react";
 import {AuthProvider} from "./context/AuthContext.tsx";
+import config from "./config.ts";
 
 const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
@@ -12,7 +13,7 @@ const root = ReactDOM.createRoot(rootElement);
 root.render(
     // <StrictMode>
     <KindeProvider clientId={"efa32183640148e780abdf87683c8ece"} domain={"https://cedata.kinde.com"}
-                   redirectUri={"http://localhost:5173"}>
+                   redirectUri={config.frontendURL}>
         <AuthProvider>
             <Router>
                 <App/>
