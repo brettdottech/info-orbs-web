817f80951010accc45643f3cecbe7c24691d3b22
Christian Erpelding <flattermann@users.noreply.github.com>
2025-03-25 20:53:47 +0100

    Add error handling note and GitHub link to WebFlashPage
    Updated the page to include an error-handling note for installation failures, suggesting users try a different USB cable. Also added a link to the GitHub repository for accessing source code and reporting issues.

--- src/pages/WebFlash/WebFlashPage.tsx
+++ src/pages/WebFlash/WebFlashPage.tsx
@@ -1,5 +1,6 @@
 import {useEffect} from "react";
 import EspWebInstallButton from "../../components/EspWebInstallButton.tsx";
+import {Link} from "react-router-dom";
 
 const WebFlashPage = () => {
     useEffect(() => {
@@ -13,7 +14,12 @@ const WebFlashPage = () => {
                 <p>This webpage allows you to flash your InfoOrbs with the latest firmware.</p>
                 <p>Simply click the below button, select your device (only Chrome & Edge supported), and then click
                     flash!</p>
-                <p>You may need to install the CH340 (USB-to-Serial) driver first.</p>
+                <p>You may need to install the USB-to-Serial driver first.</p>
+            </div>
+            <div className="mb-6">
+                <p className="font-bold">If the installation fails (e.g. with "Invalid head of packet (0xff): Possible
+                    serial noise or
+                    corruption."), please try a different USB cable.</p>
             </div>
             <EspWebInstallButton manifest="/webflash/manifest.json"
                                  buttonText="Flash InfoOrbs DEV (Default)"/>
@@ -21,6 +27,12 @@ const WebFlashPage = () => {
                                  buttonText="Flash InfoOrbs DEV (Keep Filesystem/CustomClocks)"/>
             {/*<EspWebInstallButton manifest="/webflash/manifest.json"*/}
             {/*                     buttonText="Flash InfoOrbs DEV (all Widgets)"/>*/}
+            <div className="mt-6">
+                You can download the source code and report issues in our&nbsp;
+                <Link className="link" target="_blank" to="https://github.com/brettdottech/info-orbs">
+                    GitHub repo
+                </Link>.
+            </div>
         </div>
     );
 };
